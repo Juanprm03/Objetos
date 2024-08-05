46fd52005e65f065848f143df6b07f90a8a0b64c
Juanprm03 <juanpabloruizmarin333@gmail.com>
2024-08-04 23:37:40 -0500

    Herencia y Polimorfismo -Implementación de Subclases por Juan Pablo Ruiz M

--- Empleado.js
+++ Empleado.js
@@ -7,3 +7,19 @@ class Empleado {
   
 
   }
+
+  class Gerente extends Empleado {
+
+  
+    constructor(nombre, salarioBase, bono) {
+      super(nombre, salarioBase);
+      this.bono = bono;
+    }
+}
+
+class Vendedor extends Empleado {
+    constructor(nombre, salarioBase, comision) {
+      super(nombre, salarioBase);
+      this.comision = comision;
+    }
+}
