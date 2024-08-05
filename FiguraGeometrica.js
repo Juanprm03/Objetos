ee3a7dd9602c31d41e37e560108c515d1c417c4a
Juanprm03 <juanpabloruizmarin333@gmail.com>
2024-08-04 23:24:05 -0500

    Encapsulamiento y Herencia -Implementación de métodos para la manipulación de propiedades por Juan Pablo Ruiz M

--- FiguraGeometrica.js
+++ FiguraGeometrica.js
@@ -6,6 +6,20 @@ class FiguraGeometrica{
     this.#nombre = nombre;
     this.#color = color;
   }
+  establecerNombre(nombre) {
+    this.#nombre = nombre;
+  }
 
+  obtenerNombre() {
+    return this.#nombre;
+  }
+
+  establecerColor(color) {
+    this.#color = color;
+  }
+
+  obtenerColor() {
+    return this.#color;
+  }
 
 }
