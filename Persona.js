e5a629b1ef7f6acd851439aa04108dcb65b4882e
Juanprm03 <juanpabloruizmarin333@gmail.com>
2024-08-04 22:45:09 -0500

    Encapsulamiento, implementación de clase persona por Juan Pablo Ruiz M

---
+++ Persona.js
@@ -0,0 +1,11 @@
+class Persona {
+  #nombre;
+  #edad;
+  #fechaNacimiento;
+
+  constructor(nombre, edad, fechaNacimiento) {
+    this.#nombre = nombre;
+    this.#edad = edad;
+    this.#fechaNacimiento = fechaNacimiento;
+  }
+}
